eb7b3929abb52d6e5e73ae8cbdf1c1e7a985f96f
sarmat <tsaloev.sarmat@gmail.com>
2025-11-25 13:41:10 +0300

    updating order errors in model

--- service_orders/internal/model/errors.go
+++ service_orders/internal/model/errors.go
@@ -3,5 +3,7 @@ package model
 import "errors"
 
 var (
-	ErrOrderNotFound = errors.New("order not found")
+	ErrOrderNotFound        = errors.New("order not found")
+	ErrMissingRequiredFields = errors.New("missing required fields")
+	ErrInvalidPrice         = errors.New("invalid price")
 )
