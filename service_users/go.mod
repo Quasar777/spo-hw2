90673fef6bb99afa8069fab954dc58bea3915a78
sarmat <tsaloev.sarmat@gmail.com>
2025-12-02 16:56:51 +0300

    updating user model: adding passHash, login and register requests

--- service_users/go.mod
+++ service_users/go.mod
@@ -2,4 +2,8 @@ module service_users
 
 go 1.24.4
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+)
--- service_users/internal/model/models.go
+++ service_users/internal/model/models.go
@@ -3,19 +3,34 @@ package model
 import "time"
 
 type User struct {
-	ID        int       `json:"id"`
-	Email     string    `json:"email,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID           int       `json:"id"`
+	Email        string    `json:"email,omitempty"`
+	Name         string    `json:"name,omitempty"`
+	PasswordHash string    `json:"-"`     // не отдаём наружу
+	Roles        []string  `json:"roles"` // например ["user"], ["admin"]
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
 }
 
 type CreateUserRequest struct {
-	Email     string    `json:"email,omitempty"`
-	Name      string    `json:"name,omitempty"`
+	Email        string `json:"email,omitempty"`
+	Name         string `json:"name,omitempty"`
+	PasswordHash string
+	Roles        []string
 }
 
 type UpdateUserRequest struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name,omitempty"`	
+	ID   int    `json:"id"`
+	Name string `json:"name,omitempty"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
